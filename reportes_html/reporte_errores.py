4b382b8a8f9d372a930975075b9a60d1a7163e6c
CocaContreras <oc26686@gmail.com>
2026-03-20 15:11:00 -0600

    subir en main

---
+++ reportes_html/reporte_errores.py
@@ -0,0 +1,79 @@
+import sys
+import os
+
+ruta_raiz = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(ruta_raiz)
+
+from antlr4 import *
+from LenguajeLexer import LenguajeLexer
+from LenguajeParser import LenguajeParser
+from antlr4.error.ErrorListener import ErrorListener
+
+
+# Listener de errores
+class MiErrorListener(ErrorListener):
+    def __init__(self):
+        self.errores = []
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.errores.append({
+            "linea": line,
+            "columna": column,
+            "mensaje": msg
+        })
+
+
+def procesar_errores_recursivo(lista, index, html):
+    if index >= len(lista):
+        return html
+
+    e = lista[index]
+
+    html += f"""
+    <tr>
+        <td>{e['linea']}</td>
+        <td>{e['columna']}</td>
+        <td>{e['mensaje']}</td>
+    </tr>
+    """
+
+    return procesar_errores_recursivo(lista, index + 1, html)
+
+
+def main():
+    os.makedirs(os.path.join(ruta_raiz, "reportes_html"), exist_ok=True)
+
+    input_stream = FileStream(os.path.join(ruta_raiz, "programa.leng"))
+    lexer = LenguajeLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = LenguajeParser(stream)
+
+    listener = MiErrorListener()
+    parser.removeErrorListeners()
+    parser.addErrorListener(listener)
+
+    parser.programa()
+
+    # HTML base
+    html = """
+    <html>
+    <head><title>Errores</title></head>
+    <body>
+    <h1>Errores Sintácticos</h1>
+    <table border="1">
+    <tr><th>Línea</th><th>Columna</th><th>Mensaje</th></tr>
+    """
+
+    # 🔁 Aplicar recursividad aquí
+    html = procesar_errores_recursivo(listener.errores, 0, html)
+
+    html += "</table></body></html>"
+
+    with open(os.path.join(ruta_raiz, "reportes_html", "reporte_errores.html"), "w", encoding="utf-8") as f:
+        f.write(html)
+
+    print("Reporte de errores generado")
+
+
+if __name__ == "__main__":
+    main()
