4441df07a8e57e09d64206d4d2fcf39d8db231dc
CocaContreras <oc26686@gmail.com>
2026-03-24 13:48:04 -0600

    SE DESCARGA LOS REPORTES Y .LEG

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify, send_file
 import subprocess
 import os
+import zipfile
 
 app = Flask(__name__)
 
-# Ruta base del proyecto
+
 RUTA_PROYECTO = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -32,6 +33,7 @@ def ejecutar(script):
     except Exception as e:
         return f"Excepción: {str(e)}"
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -41,14 +43,13 @@ def index():
 def analizar():
     codigo = request.json.get('codigo', '')
 
-    # Guardar código en archivo
     ruta_archivo = os.path.join(RUTA_PROYECTO, 'programa.leng')
     with open(ruta_archivo, 'w', encoding='utf-8') as f:
         f.write(codigo)
 
     print("Nuevo análisis ejecutado")
 
-    # Rutas ABSOLUTAS (clave)
+    # RUTAS DE LOS REPORTES
     ruta_tokens = os.path.join(RUTA_PROYECTO, 'static', 'reportes_html', 'reporte_tokens.py')
     ruta_errores = os.path.join(RUTA_PROYECTO, 'static', 'reportes_html', 'reporte_errores.py')
     ruta_rec = os.path.join(RUTA_PROYECTO, 'static', 'reportes_html', 'reporte_recuperables.py')
@@ -97,6 +98,33 @@ def ver_recuperables():
     if os.path.exists(ruta):
         return send_file(ruta)
     return "Primero debes analizar el código"
+
+#FUNCION DE DESCARGA
+@app.route('/descargar')
+def descargar_todo():
+    ruta_zip = os.path.join(RUTA_PROYECTO, 'compilador_resultado.zip')
+
+    with zipfile.ZipFile(ruta_zip, 'w') as zipf:
+
+        ruta_codigo = os.path.join(RUTA_PROYECTO, 'programa.leng')
+        if os.path.exists(ruta_codigo):
+            zipf.write(ruta_codigo, 'programa.leng')
+
+        #SE CREA CARPETA PARA GUARDAR LOS REPORTES
+        ruta_tokens = os.path.join(RUTA_PROYECTO, 'reportes_html', 'reporte_tokens.html')
+        ruta_errores = os.path.join(RUTA_PROYECTO, 'reportes_html', 'reporte_errores.html')
+        ruta_rec = os.path.join(RUTA_PROYECTO, 'reportes_html', 'reporte_recuperables.html')
+
+        if os.path.exists(ruta_tokens):
+            zipf.write(ruta_tokens, 'reportes/reporte_tokens.html')
+
+        if os.path.exists(ruta_errores):
+            zipf.write(ruta_errores, 'reportes/reporte_errores.html')
+
+        if os.path.exists(ruta_rec):
+            zipf.write(ruta_rec, 'reportes/reporte_recuperables.html')
+
+    return send_file(ruta_zip, as_attachment=True)
 
 
 if __name__ == '__main__':
