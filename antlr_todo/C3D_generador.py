dd58419470ef64a2bb87b9362fa665a345e35b8e
Chiza <chizaelite1280@gmail.com>
2026-04-18 22:34:31 -0600

    Recorrido de arbol para identificar expresiones

--- antlr_todo/C3D_generador.py
+++ antlr_todo/C3D_generador.py
@@ -2,8 +2,30 @@ from LenguajeVisitor import LenguajeVisitor
 
 class C3DGenerador(LenguajeVisitor):
 
+#Base del generador  
     def __init__(self):
         self.codigo = []
         self.temp_count = 0
 
+    def new_temp(self):
+        self.temp_count += 1
+        return f"t{self.temp_count}"
     
+    def emit(self, line):
+        self.codigo.append(line)
+
+    #Deviinimos las expresiones a partir del visitor
+    def visitExpr(self, ctx):
+        #si se encuentra el nodo hijo del arbol retorna el texto que contenga
+        if ctx.getChildCount() == 1:
+            return ctx.getText()
+        
+        #Recorre el arbol
+        left = self.visit(ctx.expr(0))
+        right = self.visit(ctx.expr(1))
+        op = ctx.OP().getText()
+
+        temp = self.new_temp()
+        self.emit(f"{temp} = {left} {op} {right}")
+
+        return temp
