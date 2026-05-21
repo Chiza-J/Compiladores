18fdc2740a0117d2dbe7a7fe1d4daf741f2d1ebc
Chiza <chizaelite1280@gmail.com>
2026-05-21 14:40:27 -0600

    Recuperables vuelve a mostrar en interfaz

--- static/reportes_html/reporte_recuperables.py
+++ static/reportes_html/reporte_recuperables.py
@@ -82,10 +82,8 @@ def main():
             <td>{r['sugerencia']}</td>
         </tr>"""
 
-    html = html.replace(
-    '<!-- PYTHON_INSERTA_FILAS_AQUI -->',
-    filas
-)
+    # Reemplazar el marcador que está dentro del <tbody>
+    html = html.replace('<!-- PYTHON_INSERTA_FILAS_AQUI -->', filas)
 
     with open(os.path.join(ruta_raiz, "reportes_html", "reporte_recuperables.html"), "w", encoding="utf-8") as f:
         f.write(html)
